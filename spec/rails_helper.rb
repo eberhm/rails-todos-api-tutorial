887ee8d56920cd974489269a63a78d5cbc3a9be6
Eber Herrera <eberhm@gmail.com>
2018-04-20 15:56:52 +0200

    Added secrets and more tests

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -72,7 +72,9 @@ end
 RSpec.configure do |config|
 
   # [...]
-  config.include RequestSpecHelper, type: :request
+  config.include RequestSpecHelper
+  config.include ControllerSpecHelper
+  
   # add `FactoryBot` methods
   config.include FactoryBot::Syntax::Methods
 
